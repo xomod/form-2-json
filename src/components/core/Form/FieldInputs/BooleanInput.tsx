661d2d62ede48e594e6bdd32b7d0f8b1d71d86e3
Mike Eling <mike.eling97@gmail.com>
2020-09-16 11:51:10 +0200

    :abcd: Add boolean value translations

--- src/components/core/Form/FieldInputs/BooleanInput.tsx
+++ src/components/core/Form/FieldInputs/BooleanInput.tsx
@@ -1,8 +1,23 @@
 import { MenuItem, TextField } from "@material-ui/core";
 import React from "react";
+import { useLittera } from "react-littera";
 import { Field } from "../..";
 
+const translations = {
+    true: {
+        en_US: "Yes",
+        pl_PL: "Tak",
+        de_DE: "Ja"
+    },
+    false: {
+        en_US: "No",
+        pl_PL: "Nie",
+        de_DE: "Nein"
+    },
+}
+
 const BooleanInput = ({ field, onChange }: { field: Field, onChange: (event: React.ChangeEvent<HTMLInputElement>) => void }) => {
+    const translated = useLittera(translations);
 
     return <TextField
         style={{ marginTop: "20px", marginBottom: "20px", marginRight: "10px", width: "85%" }}
@@ -14,7 +29,7 @@ const BooleanInput = ({ field, onChange }: { field: Field, onChange: (event: Rea
     >
         {[{ value: "true", label: "True" }, { value: "false", label: "False" }].map((val: { value: string, label: string }) => (
             <MenuItem key={val.label} value={val.value}>
-                {val.label}
+                {translated[val.value as "true" | "false"]}
             </MenuItem>
         ))}
     </TextField>;
